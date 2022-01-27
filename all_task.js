6e0b7494d3052dbcf0d794c6847bf5113011ade6
fedorgerasimov <1031577@mail.ru>
2022-01-27 14:53:25 +0300

    all task

---
+++ all_task.js
@@ -0,0 +1,14 @@
+// Escaping Literal Quotes in Strings
+const myStr1 = " I am a \"double quoted\" string inside \"double quotes\"."
+console.log(myStr1);
+
+//Quoting Strings with Single Quotes
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Escape Sequences in Strings
+const myStr3 = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//Find the Length of a String
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;
