97b34bf261b4b26e2918ee7a8b31cb8edbc11533
fedorgerasimov <1031577@mail.ru>
2022-01-31 15:19:41 +0300

    add default option in switch

--- all_task.js
+++ all_task.js
@@ -200,3 +200,23 @@ function caseInSwitch(val) {
   return answer;
 }
 console.log(caseInSwitch(3));
+
+//Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a":
+      answer = 'apple';
+      break;
+    case "b":
+      answer = 'bird';
+      break;
+    case "c":
+      answer = 'cat';
+      break;
+    default:
+      answer = 'stuff';
+  }
+  return answer;
+}
+console.log(switchOfStuff('b'));
