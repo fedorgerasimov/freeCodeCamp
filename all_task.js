23c2a61d55a9d3e04eeb72368afacf04bdeb7349
fedorgerasimov <1031577@mail.ru>
2022-01-31 16:13:01 +0300

    replacing if else with Switch

--- all_task.js
+++ all_task.js
@@ -244,3 +244,47 @@ function sequentialSizes(val) {
   return answer;
 }
 console.log(sequentialSizes(5));
+
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+console.log(chainToSwitch(7));
+
+function chainToSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case  42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+    default:
+      answer = "";
+  }
+  return answer;
+}
+console.log(chainToSwitch(100));
+
