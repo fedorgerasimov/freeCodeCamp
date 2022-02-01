f2ae18da68122840144220030f33541b2e66d5aa
fedorgerasimov <1031577@mail.ru>
2022-02-01 14:06:41 +0300

    record collection

--- all_task.js
+++ all_task.js
@@ -443,3 +443,60 @@ const myStorage = {
 };
 const gloveBoxContents = myStorage.car.inside["glove box"];
 console.log(gloveBoxContents);
+
+//Accessing Nested Arrays  >= pine
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+const secondTree = myPlants[1].list[1];
+console.log(secondTree);
+
+//Record Collection
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+function updateRecords(records, id, prop, value) {
+  if (prop !== 'tracks' && value !=='') {
+    records[id][prop] = value;
+  }else if (prop === 'tracks' && records[id].hasOwnProperty('tracks')=== false){
+    records[id][prop] = [value];
+  }else if (prop === 'tracks' && value !==''){
+    records[id][prop].push(value);
+  }else if (value === ''){
+    delete records[id][prop];
+  }
+  return records;
+}
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
