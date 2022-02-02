d78387a2f8451615ece1804b30fa84a357bea985
fedorgerasimov <1031577@mail.ru>
2022-02-02 13:44:33 +0300

    Pfofile Lookup

--- all_task.js
+++ all_task.js
@@ -652,5 +652,51 @@ function sum(arr, n) {
 }
 console.log(sum([2, 3, 4], 1)); //>=2
 
+/*Profile Lookup
+The function should check if name is an actual contact's firstName
+ and the given property (prop) is a property of that contact.
+If both are true, then return the "value" of that property.
+If name does not correspond to any contacts then return the string No such contact.
+If prop does not correspond to any valid properties of a contact found to match name
+then return the string No such property.
+*/
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such property";
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+
 
 
